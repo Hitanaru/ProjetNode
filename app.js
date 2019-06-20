88bbb8d17840d74df038cdec00e04abceae2535c
Hitaru <bryan.milon.lpcb@gmail.com>
2019-06-20 14:20:20 +0200

    01

---
+++ app.js
@@ -0,0 +1,359 @@
+//------------------------------------------------------------------------------------/
+//------------------- Départ du serveur forum avec le routeur ------------------------/
+//------------------------------------------------------------------------------------/
+//-----------------------------///////---///--///////////-----------------------------/
+//----------------------------///-------///------///----------------------------------/
+//---------------------------///-////--///------///-----------------------------------/
+//--------------------------///////---///////--///------------------------------------/
+//------------------------------------------------------------------------------------/
+
+
+console.log('---------------------------------------------mise en route du serveur pour le forum -----------------------------------------------------');
+
+
+//------------------------------------------------------------------------------------/
+//----------------------- Définition des modules requis ------------------------------/
+//------------------------------------------------------------------------------------/
+const 	express = require('express'),
+		app = express(),
+		bodyParser = require('body-parser'),
+		urlencodedParser = bodyParser.urlencoded({ extended: false }),
+		session = require('express-session'),
+		db = require('./db.js'),
+		ip = require("ip");
+var user_connect = 0;
+
+//------------------------------------------------------------------------------------/
+//---------------- Définition d'un dossier public pour les annexes -------------------/
+//------------------------------------------------------------------------------------/		
+app.use(express.static('public'));
+
+//------------------------------------------------------------------------------------/
+//------------------- Définition des options de la session ---------------------------/
+//------------------------------------------------------------------------------------/ 
+ app.use(session({
+	  	secret: '343ji43j4n3jn4jk3n',
+	  	resave: false,
+	  	saveUninitialized: true,
+	   	cookie: {
+	        secure: false
+	    }
+	}));
+
+//------------------------------------------------------------------------------------/
+//---------------------------- Début du routeur---------------------------------------/
+//------------------------------------------------------------------------------------/
+app
+
+//------------------------------------------------------------------------------------/
+//-------- Redirige vers la page principale en cas d'arriver avec url '/'-------------/
+//------------------------------------------------------------------------------------/
+	.get('/', function(req, res){
+	/*!!!!!!!!!!!  Travailler sur la redirection en cas de statut 404 !!!!!!!!!!!!!!*/
+    	res.redirect('/forum/index');
+	})// EO /
+
+
+//------------------------------------------------------------------------------------/
+//----------------------- Page principale du forum -----------------------------------/
+//------------------------------------------------------------------------------------/
+	.get('/forum/index', function(req, res) {
+
+		req.session.adr_ip = ip.address();
+		req.session.sujet = false;
+		req.session.user_connect = user_connect;
+
+		db.topics(function(topics){			
+			db.statistics(function(data){
+
+				res.render('index.ejs',{topics : topics, session : req.session, statistics : data });
+			
+			});// EO statistics			
+		});// EO db.sujets()
+	})// EO /index
+
+
+//------------------------------------------------------------------------------------/
+//------------------------- Page d'affichage des articles ----------------------------/
+//------------------------------------------------------------------------------------/
+	.get('/forum/papers/:id', function(req, res) {
+
+		req.session.adr_ip = ip.address();
+		req.session.topic = req.params.id;
+		req.session.user_connect = user_connect;
+
+		db.papers( req.session.topic, function(papers){
+
+			db.statistics(function(data){
+
+				res.render('papers.ejs',{papers : papers, session : req.session, statistics : data});			
+			});//EO statistics			
+		});// EO db.article
+	})// EO /article
+
+	.get('/forum/index/:id/delete', db.product_delete);
+//------------------------------------------------------------------------------------/
+//-------------------- Page de récupération des infos utilisateur --------------------/
+//------------------------------------------------------------------------------------/	
+	app.get('/forum/report', function(req, res){
+		req.session.adr_ip = ip.address();
+
+		res.render('report.ejs', {session : req.session});
+	})// EO /info
+
+
+//------------------------------------------------------------------------------------/
+//------------------------- Page d'inscription ---------------------------------------/
+//------------------------------------------------------------------------------------/
+	.get('/forum/signin', function(req, res ){
+		req.session.adr_ip = ip.address();
+
+		res.render('signin.ejs' ,{session : req.session});
+	})// EO singin
+
+
+//------------------------------------------------------------------------------------/
+//-------- Traitement du formulaire d'authentification utilisateur -------------------/
+//------------------------------------------------------------------------------------/
+	.post('/forum/login',urlencodedParser, function(req, res){
+
+		var email = req.body.email,
+			password = req.body.password;
+
+		db.authentication(email , password , function(data){
+			if (data) {
+				req.session.connection = true;
+				req.session.email = email;
+				req.session.username = data ;
+				user_connect ++;
+				console.log(user_connect + ' connecté sur le forum')
+			} else{
+				req.session.connection = false;
+				req.session.errror = 'Adresse mail ou mot de passe incorrect';				
+			}// EO if/else
+
+			if(req.session.topic){
+				res.redirect('/forum/papers/'+ req.session.topic);
+			}else{
+				res.redirect('/forum/index');
+			}// EO if/else			
+		});// EO db.authentication()
+	})// EO login
+
+
+//------------------------------------------------------------------------------------/
+//--------------------- Traitement du lien de déconnexion ----------------------------/
+//------------------------------------------------------------------------------------/
+	.get('/forum/logout', function(req, res){
+
+		req.session.connection = false;
+		req.session.username = '';
+		req.session.email = '';
+		req.session.topic = false;
+		user_connect -= 1;
+		console.log(user_connect +' Utilisateur connectés sur le forum')
+		res.redirect('/forum/index');
+		
+
+
+	})// EO logout
+
+
+//------------------------------------------------------------------------------------/
+//------------------- Traitement du formulaire d'inscription -------------------------/
+//------------------------------------------------------------------------------------/
+	.post('/forum/new_user',urlencodedParser, function(req, res){
+
+			var new_user = {
+				password : req.body.password,
+				username : '',
+				email : ''
+				} ,
+				vpassword = req.body.vpassword;				
+
+			if (new_user.password === vpassword) {
+
+				new_user.username = req.body.username;
+				new_user.email = req.body.email;
+				
+				db.signin(new_user, function(err){
+
+					if (err) {
+						console.log(err);
+						req.session.error = err;
+						res.redirect('/forum/signin');
+					} else{
+						req.session.connection = true;
+						req.session.username = new_user.username;
+						req.session.email = new_user.email;
+						res.redirect('/forum/index');
+					}// EO if/else
+				});// EO db.signin
+
+			} else{
+				req.session.error = 'verification du mot de pass incorect';
+				res.redirect('/forum/signin');
+			}// EO if/else
+	})// EO /inscription
+
+
+//------------------------------------------------------------------------------------/
+//---------------- Traitement du formulaire de nouveau sujet -------------------------/
+//------------------------------------------------------------------------------------/
+	.post('/forum/add_topic',urlencodedParser,function(req, res){
+
+		if (req.session.connection) {
+			var data = {
+					topic : req.body.title_topic,
+					paper : req.body.title_paper,
+					content : req.body.paper,
+					author : req.session.username
+				}
+			db.new_topic(data, function(err){
+				if (err) {
+					req.session.error = err ;
+					res.redirect('/forum/papers/'+ data.topic);
+				}else{
+					res.redirect('/forum/index');
+				}// EO if/else
+
+			})//EO new_topic()
+
+		} else {
+			req.session.error ='Veuillez vous connecter pour ouvrir un nouveau sujet';
+			res.redirect('/forum/index');	
+		}//EO if/else
+	})//EO /forum/add_sujet
+
+
+//------------------------------------------------------------------------------------/
+//---------------- Traitement du formulaire de nouvelle article ----------------------/
+//------------------------------------------------------------------------------------/
+	.post('/forum/add_paper/:id',urlencodedParser,function(req, res){
+		if (req.session.connection) {
+			var data = {
+					topic : req.params.id,
+					paper : req.body.title,
+					content : req.body.paper,
+					author : req.session.username
+				}
+
+			db.new_paper(data, function(err){
+				if (err) {
+					req.session.error = err ;
+					res.redirect('/forum/papers/'+ data.topic);
+				}else{
+					res.redirect('/forum/papers/'+ data.topic);
+				}// EO if/else
+
+			})// EO new_paper()
+
+		} else {
+			req.session.error ='Veuillez vous connecter pour ajouter un article';
+			res.redirect('/forum/papers/'+  req.params.id);	
+		}// EO if/else
+	})// EO /add_paper
+
+
+//------------------------------------------------------------------------------------/
+//---------------- Traitement du formulaire de nouveau commentaire -------------------/
+//------------------------------------------------------------------------------------/
+	.post('/forum/comment/:id',urlencodedParser,function(req, res){
+		
+		if (req.session.connection) {
+			var data ={
+					paper : req.params.id,
+					topic : req.session.topic,
+					comment : req.body.content,
+					author : req.session.username
+				}
+
+			db.new_comment(data, function(err){
+
+				if (err) {
+					req.session.error = err ;
+					res.redirect('/forum/papers/'+ req.session.topic);
+				}else{
+					res.redirect('/forum/papers/'+ req.session.topic);
+				}// EO if/else
+			})// EO new_comment()
+		}else {
+			req.session.error ='Veuillez vous connecter pour ajouter un nouveau commentaire';
+			res.redirect('/forum/papers/'+ req.session.sujet);
+		}// EO if/else
+	})// EO /comment
+
+
+//------------------------------------------------------------------------------------/
+//----- Traitement du formulaire de mise à jour de l'adresse email utilisateur -------/
+//------------------------------------------------------------------------------------/
+	.post('/forum/mail_update/',urlencodedParser,function(req, res){
+		if (req.session.connection) {
+		var data = {
+			new_email : req.body.email,
+			username : req.session.username
+			}
+
+			db.update_email(data, function(err){
+				if (err) {
+					req.session.error = err ;
+					res.redirect('/forum/report');
+				}else{
+					req.session.email = req.body.email;
+					res.redirect('/forum/report');
+					}// EO if/else
+			})// EO new_comment()			
+		} else{
+			req.session.error ='Vous n\47 êtes  connecter a aucun compte';
+			res.redirect('/forum/report');
+		}// EO if/else
+	})// EO mail_update
+
+
+//------------------------------------------------------------------------------------/
+//----- Traitement du formulaire de mise a jour du mot de passe utilisateur ----------/
+//------------------------------------------------------------------------------------/
+	.post('/forum/password_update/',urlencodedParser,function(req, res){
+		if (req.session.connection) {
+			var new_password1 = req.body.password;
+			var new_password2 = req.body.vpassword;
+
+			console.log(new_password1);
+			console.log(new_password2);
+
+			if (new_password1 === new_password2) {
+
+				var data = {
+					new_password : new_password2 ,
+					username : req.session.username
+					}
+
+				db.update_password(data, function(err){
+
+					if (err) {
+						req.session.error = err ;
+						res.redirect('/forum/report');
+					}else{
+						req.session.error = 'Mot de passe correctement modifié' ;
+						res.redirect('/forum/report');
+					}// EO if/else
+			})// EO new_comment()
+			}else{
+
+				   req.session.error = 'Les mots de passe tapé ne sont pas identiques';
+					res.redirect('/forum/report');
+
+			}// EO if/else
+		} else{
+			req.session.error ='Vous n\47 êtes  connecter a aucun compte';
+			res.redirect('/forum/report');
+		}// EO if/else
+	})// EO uptdate password
+
+;//EO routeur
+
+
+//------------------------------------------------------------------------------------/
+//------------- Mise en écoute du serveur sur le port '8080' -------------------------/
+//------------------------------------------------------------------------------------/
+app.listen(8080);
---
+++ db.js
@@ -0,0 +1,429 @@
+//------------------------------------------------------------------------------------/
+//------------------- Module d'accès aux donnée de la base forum ---------------------/
+//------------------------------------------------------------------------------------/
+//-----------------------------///////---///--///////////-----------------------------/
+//----------------------------///-------///------///----------------------------------/
+//---------------------------///-////--///------///-----------------------------------/
+//--------------------------///////---///////--///------------------------------------/
+//------------------------------------------------------------------------------------/
+
+
+//------------------------------------------------------------------------------------/
+//----------------------------- Appele du module mongoose ----------------------------/
+//------------------------------------------------------------------------------------/
+const mongoose = require('mongoose');
+
+
+//------------------------------------------------------------------------------------/
+//------------ Mise en place des modèles de la base de données -----------------------/
+//------------------------------------------------------------------------------------/
+const 	userSchema = new mongoose.Schema({
+			username :  String,
+			email : String,
+			password : String		 	
+			}),
+		userModel = mongoose.model('users', userSchema),
+
+		topicSchema = new mongoose.Schema({
+			 	title : String,
+			 	date : { type : Date, default : Date.now },			 	
+				author :  String
+			}),
+		topicModel = mongoose.model('topics', topicSchema),
+
+		paperSchema = new mongoose.Schema({
+			  title : String,
+			  content : String,
+			  date : { type : Date, default : Date.now },			  			  
+			  author :  String,
+			  topic : String,	  
+
+			  comment : [{
+			  	content : String,
+			  	author : String,
+			  	date : { type : Date, default : Date.now }
+			 	 }
+			  ]
+			}),
+		paperModel = mongoose.model('papers', paperSchema)
+;//EO mep modèles
+
+
+//------------------------------------------------------------------------------------/
+//--------------- Méthode de connexion à la base de données --------------------------/
+//------------------------------------------------------------------------------------/
+connect2Db = function(callBack){
+	mongoose.connect('mongodb://localhost/forum',function(err, db) {
+		if (err) {
+		  	console.log(err);
+		    console.log("Connexion à la base de données 'Forum' échoué");
+
+		}else{
+		    callBack();
+		}// EO if/else
+	});// EO mongoose.connect
+};// EO connect2Db()
+
+
+//------------------------------------------------------------------------------------/
+//--------------- Méthode de déconnexion de la base de données -----------------------/
+//------------------------------------------------------------------------------------/
+disconnect2Db = function(){
+	mongoose.connection.close(function (err){
+		if (err) {
+			 console.log(err);
+			 console.log('Deconnexion échoué')
+		}// EO if		
+	});// EO mongoose.connection.close
+};// EO disconnect2Db
+
+
+//------------------------------------------------------------------------------------/
+//------------------------ Méthode d'accès aux sujets --------------------------------/
+//--------------------------retourne tous les sujets----------------------------------/
+//------------------------------------------------------------------------------------/
+topics = function(callBack){
+
+	connect2Db( function(){
+		topicModel.find(null, function (err, topics) {	
+			if (err){
+				console.log(err);
+
+			}else{
+				disconnect2Db();
+				return callBack(topics);		
+			}// EO if/else		
+		});// EO sujetModel.find
+	});// EO connect2Db()
+};// EO exports.sujet
+module.exports.topics = topics; 
+
+//------------------------------------------------------------------------------------/
+//------------------------ Méthode d'accès aux articles ------------------------------/
+//------- Retourne les articles dont le titre du sujet est passé en paramètre --------/
+//------------------------------------------------------------------------------------/
+papers = function(data , callBack){
+
+	connect2Db(function(){
+
+			paperModel.find({topic : data},function(err, papers){
+
+				if (err) {
+					console.log(err);
+				}else{					
+					return callBack(papers);
+				}// EO if/else
+			});// EO find()
+
+	});// EO connect2Db()
+};// EO papers()
+module.exports.papers = papers; 
+
+//------------------------------------------------------------------------------------/
+//------------------------ Méthode de supression des articles ------------------------/
+//------------------------------------------------------------------------------------/
+
+exports.product_delete = function (req, res) {
+    topicModel.findByIdAndRemove(req.params.id, function (err) {
+        if (err) return (err);
+        res.send('Deleted successfully!');
+    })
+};
+
+//------------------------------------------------------------------------------------/
+//----------------------- Méthode de récupération des statistiques -------------------/
+//------------------------------------------------------------------------------------/
+statistics = function(callBack){
+	var statistics = {
+			last_paper : '',
+			nbT_topics : 0,
+			nbT_papers : 0,
+			nbT_users : 0,
+			paper_more_comment : '',
+			last_user : ''
+		}
+		var paper_ = {
+			title :'',
+			topic : '',
+			nt_comment : ''
+						}
+		var nbC = 0;
+
+	connect2Db(function(){
+		userModel.find(null, function(err,users){
+			if(users.length < 1){
+				statistics.nbT_users = 0
+			}else{
+				user = users[users.length-1];
+				statistics.last_user = user.username;
+			}
+
+			paperModel.find(null,function(err, papers){
+
+				statistics.nbT_papers = papers.length;
+
+				if(papers.length < 1){
+					statistics.nbT_papers = 0;
+				}
+				else{
+					statistics.last_paper = papers[papers.length-1];
+				}
+
+				for (var i = 0; i < papers.length; i++) {
+					var paper = papers[i];
+					var comment = paper.comment; 
+
+
+					if (comment.length > nbC) {
+						nbC = comment.length;
+						paper_ = {
+							title : paper.title,
+							topic : paper.topic,
+							nt_comment : comment.length
+						}						
+
+					}// EO if
+				}// EO for
+
+				topicModel.find(null, function (err, topics) {
+
+					statistics.paper_more_comment  = paper_ ;
+					statistics.nbT_topics = topics.length ;
+					callBack(statistics);
+
+				});// EO sujetModel
+			});// EO articleModel
+		});// EO userModel 
+	});//EO connect2Db
+}//EO statistics
+module.exports.statistics = statistics;
+
+
+//------------------------------------------------------------------------------------/
+//----------------------- Méthode d'authentification ---------------------------------/
+//------------------------------------------------------------------------------------/
+authentication = function(email, password, callBack){
+
+	connect2Db(function(){
+
+		var username;
+		userModel.find({email : email},function(err, users){
+
+			if (users == '') { 
+				console.log('Adresse mail inconnue') ;
+				return callBack(false);
+
+			} else {
+				var user = users[0];
+
+				if (password === user.password){
+
+				console.log(user.username + ' vient de se connecté');
+				username = user.username;
+				callBack(username);
+
+				}else {
+
+				console.log('Mot de passe incorrect');
+				return callBack(false);
+
+				}// EO if/else
+			}// EO if/else
+		});// EO find()
+	})// EO connect2Db()
+};// EO authentication()
+module.exports.authentication = authentication; 
+
+
+//------------------------------------------------------------------------------------/
+//---------------- Méthode d'inscription sur la base de données ----------------------/
+//------------------------------------------------------------------------------------/
+signin = function(new_user, callBack){
+	connect2Db(function(){
+
+
+		userModel.find({'username' : new_user.username},function(err, users){
+			if (users == '') {
+
+				userModel.find({'email' : new_user.email},function(err, users){
+
+					if (users == '') {
+
+						var user = new userModel();
+
+						user.username = new_user.username;
+						user.email = new_user.email;
+						user.password = new_user.password;
+						user.save(function (err) {
+							if (err) { 
+								throw err; 
+							}else{
+								return callBack(false);
+								disconnect2Db();
+							}//EO if/else
+						});//EO user.save
+					}else{
+						return callBack('Cette adresse mail est déjà utilisée');
+						disconnect2Db();
+					}//EO if/else
+				})//EO query.exec()
+
+			} else {
+				return callBack('Ce pseudo est déjà utilisé');
+				disconnect2Db();
+			}// EO if/else
+		});// EO find()
+	});// EO connect2Db()
+};// EO export.signin()
+module.exports.signin = signin; 
+
+
+//------------------------------------------------------------------------------------/
+//---------------------- Méthode d’ajouts de nouveau sujets --------------------------/
+//------------------------------------------------------------------------------------/
+new_topic = function(data , callBack){
+	connect2Db(function(){
+
+		topicModel.find({title : data.topic},function(err, topics){
+			console.log(topics);
+			if (topics == '') {
+				var topic = new topicModel();
+				topic.title = data.topic;
+				topic.author = data.author;
+
+				topic.save(function(err){
+					if (err) {
+						return callBack('Erreur de sauvegarde');
+						disconnect2Db();
+					}else{
+						new_paper(data , function(err){
+							if (err) {
+								return callBack(err);
+							}else{
+								return callBack(false);
+							}// EO if/else
+						});// EO new_paper
+					}// EO if/else
+				});// EO sujet save
+			}else{
+				return callBack('Se sujet a déjà été ouvert');
+			}// EO if/else
+		});// EO find
+	});// EO connect2Db()
+};// EO new_topic()
+module.exports.new_topic = new_topic;
+
+
+//------------------------------------------------------------------------------------/
+//------------------ Méthode d'ajouts de nouveaux articles ---------------------------/
+//------------------------------------------------------------------------------------/
+new_paper = function(data, callBack){
+
+	connect2Db(function(){
+
+		paperModel.find({title : data.paper, topic : data.topic},function(err, papers){
+			if (err) {
+				console.log(err);
+			}else{
+				if (papers == '') {
+
+					var paper = new paperModel();
+					paper.title = data.paper;
+					paper.topic = data.topic;
+					paper.author = data.author;
+					paper.content = data.content;
+
+					paper.save(function(err){
+						if (err) {
+							return callBack(err);
+						}else{
+							return callBack(false);
+						}// EO if/else
+					})// EO article.save
+				}else{
+					return callBack('Ce titre d \47 article est déjà utilisé dans ce sujet ');
+				}
+			}// EO if/else
+		})// EO if/else
+	});// EO connect2Db()
+}// EO new_paper()
+module.exports.new_paper = new_paper;
+
+
+//------------------------------------------------------------------------------------/
+//------------------ Méthode d'ajout d'un nouveau commentaire ------------------------/
+//------------------------------------------------------------------------------------/
+new_comment = function(data, callBack){
+
+	connect2Db(function(){
+
+		paperModel.find({title : data.paper, topic : data.topic},function(err, papers){
+			if (err) {
+				console.log(err);
+				return callBack(err);
+			}else{
+				var new_comment =  {
+					content : data.comment,
+					author : data.author
+					},
+
+					paper = papers[0],
+					comment = [];
+
+				comment = paper.comment;
+				comment.push(new_comment);
+
+				paperModel.update({title : data.paper, topic : data.topic}, { comment : comment }, { multi : true }, function (err) {
+				 		if (err) { 
+				 			return callBack(err);
+				 		}else{
+							return callBack(false);
+				 		}
+				});// EO update
+			}// EO if/else
+		}); // EO find()
+	});// EO connect2Db()
+}// EO new_comment
+module.exports.new_comment = new_comment;
+
+
+//------------------------------------------------------------------------------------/
+//------------------ Méthode de mise a jour de l'email utilisateur -------------------/
+//------------------------------------------------------------------------------------/
+update_email = function(data, callBack){
+
+	connect2Db(function(){
+		userModel.find({email : datat.new_email},function(err, users){
+			if (users == '') {
+				userModel.update({ username : data.username}, { email : data.new_email }, { multi : true }, function (err) {
+	 				if (err) { 
+	 					return callBack(err);
+	 				}else{
+	 					return callBack(false);
+	 				}// EO if/else
+				});// EO update
+			}else{
+				return callBack('Cette adresse mail est déjà utilisée');
+			}
+		});// EO find
+	});// EO connect
+}// EO user_update
+module.exports.update_email = update_email;
+
+
+//------------------------------------------------------------------------------------/
+//--------------- Méthode de mise a jour du mot de passe utilisateur -----------------/
+//------------------------------------------------------------------------------------/
+update_password = function(data, callBack){
+
+	connect2Db(function(){
+		userModel.update({ username : data.username}, { password : data.new_password }, { multi : true }, function (err) {
+	 		if (err) { 
+	 			return callBack(err);
+	 		}else{
+	 			return callBack(false);
+	 		}//EO if/else
+		});//EO update
+	});//EO connect2Db
+}//EO user_update
+module.exports.update_password = update_password;
